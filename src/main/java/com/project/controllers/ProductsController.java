57d3478567a511826a988a5c1079099b240c3497
Luu Minh Sang <luuminh.sang0010@gmail.com>
2022-05-13 22:54:11 +0700

    add 2 line

--- src/main/java/com/project/controllers/ProductsController.java
+++ src/main/java/com/project/controllers/ProductsController.java
@@ -23,6 +23,7 @@ public class ProductsController {
     @GetMapping("/ProductList/{brand}")
     public ModelAndView products(@PathVariable(value ="brand") Integer id, HttpServletRequest request) {
         System.out.println("add 1 line");
+        System.out.println("add 1 line");
         List<Product> products = productsRepository.findProductByBrandId(id);
         return new ModelAndView("product-list");
     }
